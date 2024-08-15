23bd46c070d538374e2e32685569b02a83470ef3
C1310810 Francisca das Chagas dos Santos <c1310810@interno.bb.com.br>
2024-08-15 15:35:06 -0300

    fim modulo 2

--- arrays/lista.js
+++ arrays/lista.js
@@ -26,8 +26,49 @@ console.log("O valor de valorA é:", valorA);
 console.log("O valor de valorB é:", valorB);
 
 // for clássico
+// let indice = 0 : executa apenas uma vez
+// indice < notas.length: condição da execução
+// indice++: executada no final de um bloco
+
 const notas = [10, 8.5, 5, 6.5, 8, 7.5];
 
 for (let indice = 0; indice < notas.length; indice++) {
   console.log(indice, notas[indice]);
 }
+
+// Calculando média com for
+// media for
+const notasMedia = [10, 6.5, 8, 7.5];
+
+let somaNotas = 0;
+
+for (let index = 0; index < notasMedia.length; index++) {
+  somaNotas += notasMedia[i];
+}
+
+const mediaNota = somaNotas / notasMedia.length;
+console.log(`Media é ${mediaNota}`);
+
+const notasM = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let i = 0; i < notasM.length; i++) {
+  somaDasNotas += notasM[i];
+}
+
+const mediaM = somaDasNotas / notasM.length;
+
+console.log(`A média das notas é ${mediaM}.`);
+
+// for of
+const numeros = [5, 8, 6, 10, 7];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+
+const media = soma / numeros.length;
+
+console.log("A média dos números é:", media);
